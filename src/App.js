bfffbd75d0aa661c8b3acc45ba6b418a2f419b1b
Shengyu Li <shspku365@gmail.com>
2021-05-21 21:38:12 +0800

    增加作者、题目、朝代。等会布局一下

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import "./styles.css";
+import { Component } from "react";
 
 const shi = require("jinrishici");
 
-export default function App() {
-  let ci = "";
-  shi.load((result) => {
-    ci = result.data.content;
-    console.log(result);
-  });
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>{ci}</h2>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ci: "",
+      author: "",
+      title: "",
+      dynasty: ""
+    };
+  }
+  // origin: Object
+  // title: "霜叶飞·重九"
+  // dynasty: "宋代"
+  // author: "吴文英"
+  componentDidMount() {
+    shi.load((result) => {
+      this.setState({
+        ci: result.data.content,
+        author: result.data.origin.author,
+        title: result.data.origin.title,
+        dynasty: result.data.origin.dynasty
+      });
+      console.log(result);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="ci">{this.state.ci}</div>
+        <div className="time">
+          <div>{this.state.dynasty}</div>
+          <div>{this.state.author}</div>
+        </div>
+        <div className="title">{this.state.title}</div>
+      </div>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import Shici from "./test";
+// import Shici from "./test";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <Shici />
+    <App />
   </StrictMode>,
   rootElement
 );
