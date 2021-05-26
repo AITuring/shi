953c90fe74b8754bd48237e66e916ac321c22a97
AITuring <shspku365@gmail.com>
2021-05-26 08:02:13 +0800

    想用路由,有点问题,晚上再说

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+// import {Router, Route} from 'react-router';
 
-// import App from "./App";
+import App from "./App";
 import Test from "./test";
+import './styles.css'
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <Test />
+    {/* <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <Route path="test" component={Test}/>
+      </Route>
+    </Router> */}
   </StrictMode>,
   rootElement
 );
